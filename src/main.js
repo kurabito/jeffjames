14d737d84dd0fcc3f56f95eb23b56fa5f278191e
Jeff James <jeffjames@gmail.com>
2021-08-09 11:34:02 -0700

    Add vue-router package

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import router from './router'
+import createRouter from './router';
+
+createApp(App).use(createRouter()).mount('#app')
