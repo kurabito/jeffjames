50fe7ad81099e0bbcf39163c777dc929d8069b3c
Jeff James <jeffjames@gmail.com>
2021-08-17 16:24:41 -0700

    Rename home page image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <a href="/"><img src="../assets/matia2014082.jpg" border="0" alt="Jeff James" /></a>
+      <a href="/"><img src="../assets/matia201408.jpg" border="0" alt="Jeff James" /></a>
   </div>
 </template>
 
