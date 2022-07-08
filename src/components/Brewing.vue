16b7f2ab9b8a4a559a53efeb9197086153c2de1f
Jeff James <jeffjames@gmail.com>
2022-07-07 20:53:55 -0700

    Add Denny Creek Brewing

--- src/components/Brewing.vue
+++ src/components/Brewing.vue
@@ -8,6 +8,9 @@
   <div>
     <h2><a href="https://www.jamesbrewing.com" target="_blank">James Brewing</a></h2>
   </div>
+  <div>
+    <h2><a href="http://dennycreekbrewing.com" target="_blank">Denny Creek Brewing</a></h2>
+  </div>
 </template>
 
 <script>
