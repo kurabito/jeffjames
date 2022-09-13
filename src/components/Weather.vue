6f81189972f1b821f370d22ba87848a7ac94f716
Jeff James <jeffjames@gmail.com>
2022-09-13 16:58:49 -0700

    Add replace to Weather router-link

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -44,7 +44,7 @@
     // function displayForecast1(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }
 
 function displayForecast2 (d, s, id) {
-    console.log("In displayForecast2");
+    // console.log("In displayForecast2");
     if (d.getElementById(id)) {
         if (window.__TOMORROW__) {
             window.__TOMORROW__.renderWidget();
