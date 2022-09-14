56160fc74989801776e67afd185b68cd93f65bae
Jeff James <jeffjames@gmail.com>
2022-09-13 20:42:23 -0700

    Move weather widget to weather.html

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -2,31 +2,21 @@
     <div class="clear">
         <h2>Burien Weather</h2>
 
-        <div class="tomorrow"
-           data-location-id="318001"
-           data-language="EN"
-           data-unit-system="IMPERIAL"
-           data-skin="light"
-           data-widget-type="upcoming"
-           style="padding-bottom:22px;position:relative;"
-        >
-          <a
-            href="https://www.tomorrow.io/weather/"
-            rel="nofollow noopener noreferrer"
-            target="_blank"
-            style="position: absolute; bottom: 0; transform: translateX(-50%); left: 50%;"
-          >
-            <img
-              alt="Powered by Tomorrow.io"
-              src="https://weather-website-client.tomorrow.io/img/powered-by-tomorrow.svg"
-              width="140"
-              height="15"
-            />
-          </a>
-        </div>
-
         <!-- <a class="weatherwidget-io" href="https://forecast7.com/en/47d61n122d33/seattle/?unit=us" data-label_1="SEATTLE"
             data-label_2="WEATHER" data-theme="original"></a> -->
+
+
+<!-- <div id="ww_4a94d603a1e85" v='1.20' loc='id' a='{"t":"responsive","lang":"en","ids":["wl3620"],"cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","sl_tof":"7","sl_sot":"fahrenheit","sl_ics":"one_a","font":"Arial"}'>Weather Data Source: <a href="https://sharpweather.com/weather_seattle/" id="ww_4a94d603a1e85_u" target="_blank">sharpweather.com/weather_seattle/</a></div><script async src="https://srv1.weatherwidget.org/js/?id=ww_4a94d603a1e85"></script> -->
+
+        <iframe title="Forecast" height="200" width="1200" 
+            style="border: none;"
+            src="https://jeffjames.net/weather.html">
+        </iframe>
+
+            <!-- src="http://localhost:8080/weather.html"> -->
+
+            <!-- src="https://forecast.weather.gov/MapClick.php?lon=-122.36193&lat=47.46077#.YyE83nbMLIV"> -->
+
 
         <iframe title="AQI" height="340" width="230"
             src="https://widget.airnow.gov/aq-dial-widget/?city=Seattle&state=WA&country=USA&transparent=true"
@@ -37,42 +27,45 @@
 
 <script>
 
+// const host = location.hostname;
+
 // WeatherWidget.io
 
     // !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js');
 
     // function displayForecast1(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }
 
-function displayForecast2 (d, s, id) {
-    // console.log("In displayForecast2");
-    if (d.getElementById(id)) {
-        if (window.__TOMORROW__) {
-            window.__TOMORROW__.renderWidget();
-        }
-        return;
-    }
-    const fjs = d.getElementsByTagName(s)[0];
-    const js = d.createElement(s);
-    js.id = id;
-    js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
+// function displayForecast2 (d, s, id) {
+//     console.log("In displayForecast2");
+//     if (d.getElementById(id)) {
+//         if (window.__TOMORROW__) {
+//             window.__TOMORROW__.renderWidget();
+//         }
+//         return;
+//     }
+//     const fjs = d.getElementsByTagName(s)[0];
+//     const js = d.createElement(s);
+//     js.id = id;
+//     js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
 
-    fjs.parentNode.insertBefore(js, fjs);
-}
+//     fjs.parentNode.insertBefore(js, fjs);
+// }
 
 // displayForecast2(document, 'script', 'tomorrow-sdk')
 
-export default {
-    name: 'Weather',
+// export default {
+//     name: 'Weather',
 
-    mounted() {
-        displayForecast2(document, 'script', 'tomorrow-sdk');
-        this.$forceUpdate();
-    },
+//     mounted() {
+//         // displayForecast2(document, 'script', 'tomorrow-sdk');
+//         // this.$forceUpdate();
+//         // this.location.reload();
+//     },
 
-    // updated() {
-    //     displayForecast2(document, 'script', 'tomorrow-sdk')
-    // },
-}
+//     // updated() {
+//     //     displayForecast2(document, 'script', 'tomorrow-sdk')
+//     // },
+// }
 
 </script>
 
