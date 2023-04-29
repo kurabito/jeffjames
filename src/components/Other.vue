718a1b24f7d31f9daedd5c7855c69a2d8c344d70
Jeff James <jeffjames@gmail.com>
2023-04-28 23:00:59 -0700

    Add Other

---
+++ src/components/Other.vue
@@ -0,0 +1,20 @@
+<!-- <template>
+    <div>
+      <img src="https://jeffjames.s3.us-west-2.amazonaws.com/sunset2011.jpg" border="0" alt="Sunset" />
+    </div>
+    <div>
+      <h2><router-link to="/weather">Weather</router-link></h2>
+    </div>
+    <div>
+      <h2><router-link to="/clock">Clock</router-link></h2>
+    </div>
+    <div>
+      <h2><router-link to="/wordle">Wordle Tool</router-link></h2>
+    </div>
+</template> -->
+  
+  <!-- <script>
+  export default {
+    name: 'Other'
+  }
+  </script> -->
