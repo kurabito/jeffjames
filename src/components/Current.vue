8be9d0ec291c0391286b3df0e084e7fb5acf6dcc
Jeff James <jeffjames@gmail.com>
2023-10-22 21:44:54 -0700

    Add calendar to Current

--- src/components/Current.vue
+++ src/components/Current.vue
@@ -1,40 +1,24 @@
 <template>
     <div class="clear">
-        <!-- <h2>Burien Time and Weather</h2> -->
 
         <div id="clock">
             <Clock />
         </div>
 
-        <!-- <h3>Tomorrow.io Forecast</h3> -->
-        <!-- <iframe title="Forecast" height="200" width="900" 
-            style="border: none;"
-            src="https://jeffjames.s3.us-west-2.amazonaws.com/weather.html">
-        </iframe> -->
         <Forecast />
 
         <div id="current">
-            <!-- <h3>Current Conditions from NOAA and EPA</h3> -->
-            <!-- <div id="current-noaa">
-                <h3>Conditions at SEA</h3>
-                 <h4>{{ currentTemp }}&deg; F</h4>
-                <h4>{{ currentWeather }}</h4>
-                <span class="left">Wind Speed: {{ windSpeed }}</span><br />
-                <span class="left">Wind Direction: {{ windDirection }}</span> -->
-                <!-- <CurrentNOAA /> 
-            </div> -->
             <iframe id="aqi" title="AQI" height="230" width="230"
                 src="https://widget.airnow.gov/aq-dial-widget/?city=Seattle&state=WA&country=USA&transparent=true"
                 style="border: none; border-radius: 25px;">
             </iframe>
 
-            <iframe src="https://embed.waze.com/iframe?zoom=10&lat=47.45&lon=-122.3&ct=livemap" width="600" height="230" allowfullscreen></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?height=230&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=amVmZmphbWVzQGdtYWlsLmNvbQ&src=MjZ0azZncGc1NDVuNTExbG9mOW03a2tqbTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=NWpqZzIzY21ydDQ5MG01M2ppbzRyb2EyMmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=OHRtNmo4MWE0cGk5ZXVuZmRmZGdqbDFoZjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=dGhvcmx5QGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=cGpndjhtMmhtMGpuZDlzYWhlcG9xMjFqMzI5cmVmMzNAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=MDYzZmJqc3Fia2Q5dGx1ODRjZDk4bG01NDhsZDRrOWtAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=bnl0aW1lcy5jb21fODlhaTRpanBiNzMzZ3QyOHJnMjFkMmMyZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%239E69AF&color=%23D50000&color=%23F6BF26&color=%23A79B8E&color=%23E67C73&color=%2333B679&color=%23616161&color=%23039BE5&color=%23D50000&color=%233F51B5" style="border:solid 1px #777" width="400" height="230" frameborder="0" scrolling="no"></iframe>
+
+            <iframe src="https://embed.waze.com/iframe?zoom=10&lat=47.45&lon=-122.3&ct=livemap" width="500" height="230" allowfullscreen></iframe>
 
             <!-- <AQI /> -->
         </div>
-
-        <div class="spacer"></div>
-
     </div>
 </template>
 
@@ -57,26 +41,11 @@ export default {
     },
 
     methods: {
-        setRefreshCurrent (interval) {
+        reloadPage (interval) {
             setInterval(() => {
                 location.reload(true)
             }, interval)
         },
-
-        // updateWeather (interval) {
-        //     setInterval(() => {
-        //         // Axios
-        //         //     .get('https://forecast.weather.gov/MapClick.php?lat=47.46551607542392&lon=-122.34916463265634&FcstType=json')
-        //         //     .then(response => (this.noaaData = response.data,
-        //         //         this.currentTemp = this.noaaData.currentobservation.Temp,
-        //         //         this.currentWeather = this.noaaData.currentobservation.Weather,
-        //         //         this.windSpeed = this.noaaData.currentobservation.Winds,
-        //         //         this.windDirection = this.noaaData.currentobservation.Windd,
-        //         //         this.periods = this.noaaData.time.startPeriodName.length));
-
-        //         CurrentNOAA.updateWeather()
-        //     }, interval)
-        // }
     },
 
     data () {
@@ -90,18 +59,19 @@ export default {
         }
     },
 
-    // mounted() {
-    //     // Axios
-    //     //     .get('https://forecast.weather.gov/MapClick.php?lat=47.46551607542392&lon=-122.34916463265634&FcstType=json')
-    //     //     .then(response => (this.noaaData = response.data,
-    //     //         this.currentTemp = this.noaaData.currentobservation.Temp,
-    //     //         this.currentWeather = this.noaaData.currentobservation.Weather,
-    //     //         this.windSpeed = this.noaaData.currentobservation.Winds,
-    //     //         this.windDirection = this.noaaData.currentobservation.Windd,
-    //     //         this.periods = this.noaaData.time.startPeriodName.length));
-    //     // this.setRefreshCurrent(60000)
-    //     this.updateWeather(60000)
-    // },
+    mounted() {
+        // Axios
+        //     .get('https://forecast.weather.gov/MapClick.php?lat=47.46551607542392&lon=-122.34916463265634&FcstType=json')
+        //     .then(response => (this.noaaData = response.data,
+        //         this.currentTemp = this.noaaData.currentobservation.Temp,
+        //         this.currentWeather = this.noaaData.currentobservation.Weather,
+        //         this.windSpeed = this.noaaData.currentobservation.Winds,
+        //         this.windDirection = this.noaaData.currentobservation.Windd,
+        //         this.periods = this.noaaData.time.startPeriodName.length));
+        // this.updateWeather(60000)
+
+        this.reloadPage(3600000)
+    },
 }
 
 </script>
@@ -113,6 +83,9 @@ export default {
     h4 {
         margin-top: auto;
         margin-bottom: auto;
+    }
+    iframe {
+        margin-right: 10px;
     }
     #clock {
         /* float: left; */
